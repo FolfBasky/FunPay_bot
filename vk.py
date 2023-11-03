039c1bbedb214f4e5e1591f0d55e7c0f4abf1a32
folfbasky <folfbasky@gmail.com>
2023-11-03 10:28:10 +0300

    y

--- vk.py
+++ vk.py
@@ -62,7 +62,7 @@ def get_admins_groups():
         )
     #result = response.json()['response']['items']
     try:
-        result = [174746452,215991768,218599370, 218819870, 218819887]
+        #result = [174746452,215991768,218599370, 218819870, 218819887]
         #for x in :
         #    result.remove(x)
         #result = 
