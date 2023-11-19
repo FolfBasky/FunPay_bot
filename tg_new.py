10ce5e407f15224eb10d963e730786e6214fb325
folfbasky <folfbasky@gmail.com>
2023-11-19 23:42:06 +0300

    y

--- tg_new.py
+++ tg_new.py
@@ -102,18 +102,19 @@ async def process_callback_button1(callback_query: types.CallbackQuery):
 @dp.message_handler(commands='confirm' , state=Clear_groups_states.select_mode)
 async def clear_groups(message: types.Message, state: FSMContext):
     global mode_clear_groups
-    try:
-        accs = select_all_vk_profiles()
-        for acc in accs:
-            set_active_status_vk_accounts()
-            choice_active_status_vk_account(acc['login'])
-            group = [x for x in groups().keys()]  
-            response = lock_all(group, lock_mode = mode_clear_groups.mode)
-            await message.answer(f'On the account {acc["login"]}: {response.lower()}')
-    except Exception as e:
-        await message.answer(e)
-    finally:
-        await state.finish()
+    #try:
+    await message.answer('Start clearing... Mode: ' + mode_clear_groups.mode)
+    accs = select_all_vk_profiles()
+    for acc in accs:
+        set_active_status_vk_accounts()
+        choice_active_status_vk_account(acc['login'])
+        group = [x for x in groups().keys()]  
+        response = lock_all(group, lock_mode = mode_clear_groups.mode)
+        await message.answer(f'On the account {acc["login"]}: {response.lower()}')
+    #except Exception as e:
+    #    await message.answer(e)
+    #finally:
+    await state.finish()
 
 
 
